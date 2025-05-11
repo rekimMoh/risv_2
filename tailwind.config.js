4ded6afd1d092dd63b27b68f167704a74c941610
rekimMoh <47531166+rekimMoh@users.noreply.github.com>
2025-05-11 10:07:20 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+             minHeight: {
+                'screen-minus-10': 'calc(100vh - 10rem)',
+              },
         },
     },
 
